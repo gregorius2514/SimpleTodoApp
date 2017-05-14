da8fde37cb1aac6d062cb65a300ba7f2681b22ce
Grzegorz <you@example.com>
2017-05-14 22:38:33 +0200

    Basic functionality of todo-app

--- src/App.js
+++ src/App.js
@@ -9,47 +9,87 @@ class App extends Component {
         super();
 
         this.state = {
-            events: []
+            notes: [],
+            noteName: ''
         }
-    }
-
-    handleSubmit(event) {
-        // this.state.comments.push(event)
-        console.log(event);
     }
     componentDidMount() {
         this.getRepos();
 
     }
+
     getRepos() {
         axios.get("http://jsonplaceholder.typicode.com/posts/1/comments").then(response => {
-            this.setState({ events: response.data });
+            this.setState({ notes: response.data });
         });
     }
 
+    saveNote() {
+        if (this.state.noteName === '') {
+            return;
+        }
+        let notes = this.state.notes.slice();
+        notes.push({
+            name: this.state.noteName
+        });
+        this.setState({
+            notes: notes,
+            noteName: ''
+        });
+
+    }
+
+    deleteNote(note) {
+        var array = this.state.notes;
+        var index = array.indexOf(note)
+        array.splice(index, 1);
+        this.setState({ notes: array });
+    }
+
+    updateTextValue(event) {
+        this.setState({
+            noteName: event.target.value
+        })
+    }
+
+    setNoteAsDone(event) {
+        console.log(event.target);
+    }
+
     render() {
-        let comments = this.state.events.map(function (item) {
+        let comments = this.state.notes.map((item) => {
             return <div id={item.postId} className="note">
                 <div className="title">{item.name}</div>
-                <div className="bodyText">{item.body}</div>
-                <div className="email">{item.email}</div>
+                <div className="remove-note-buttons-left" onClick={(event) => this.setNoteAsDone(event)}>
+                    <a href="#">
+                        <i className="glyphicon glyphicon-ok"></i>
+                    </a>
+                </div>
+                <div className="remove-note-buttons-right" onClick={(event) => this.deleteNote(item)}>
+                    <a href="#">
+                        <i className="glyphicon glyphicon-remove"></i>
+                    </a>
+                </div>
             </div>
         });
 
         return (
-            <div className="App">
-                <div className="App-header">
-                    <h2>Simple app</h2>
-                </div>
-                <div>
+            <div className="i-am-centered">
+                <div className="row">
+                    <h1>Simple note app</h1>
+                    <div className="input-group note">
+                        <input className="form-control" type='text' placeholder="Your's new note" value={this.state.noteName}
+                            onChange={(event) => this.updateTextValue(event)} />
+                        <span className="input-group-btn">
+                            <button className="btn btn-default" type="button" onClick={(event) => this.saveNote()}>
+                                <i className="glyphicon glyphicon-saved"></i>
+                            </button>
+                        </span>
+                    </div>
                     <div>
                         {comments}
                     </div>
                 </div>
-
-                <form id="add-form" onSubmit={this.handleSubmit}>
-                    <input id='create' ref="description" type='text' placeholder='Add Something to the list!' />
-                </form>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(
   <App />,
